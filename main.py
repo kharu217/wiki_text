80d8427d4401eecd7bcfe5f18073e5339d4fbb41
kharu217 <kharu217.dev@gmail.com>
2025-07-26 03:04:03 +0200

    commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import time
 
 is_first = True
 strain = bs4.SoupStrainer("p")
-page_count = 18066
+page_count = 24390
 
 def get_re(url) :
     result_t = ""
@@ -33,7 +33,7 @@ def get_re(url) :
 
 if __name__ == "__main__" :
     freeze_support()
-    page_url = 'https://en.wikipedia.org/wiki/Special:AllPages?from=Folksworth_and_Washingley&to=&namespace=0'
+    page_url = 'https://en.wikipedia.org/wiki/Special:AllPages?from=James_Obiorah&to=&namespace=0'
     while page_url != "https://en.wikipedia.org/wiki/Special:AllPages/0" or is_first:
         start = time.time()
         page_count += 1
@@ -55,55 +55,5 @@ if __name__ == "__main__" :
                 f.write(result)
             time.sleep(0.01)
         print(f"{time.time()-start}s")
+        print(page_count)
         page_url = "https://en.wikipedia.org" + page_soup.find_all('a', title="Special:AllPages")[1]["href"]
-=======
-import bs4
-from bs4 import BeautifulSoup
-import requests
-import re
-from tqdm import tqdm
-
-is_first = True
-strain = bs4.SoupStrainer("p")
-page_count = 0
-
-def get_re(url) :
-    while True :
-        try :
-            response = requests.get(url)
-            soup = BeautifulSoup(response.text, "lxml",parse_only=bs4.SoupStrainer("p"))
-            return soup
-        except KeyboardInterrupt :
-            raise KeyboardInterrupt
-        except :
-            continue
-
-
-page_url = "https://en.wikipedia.org/wiki/Special:AllPages/0"
-while page_url != "https://en.wikipedia.org/wiki/Special:AllPages/0" or is_first:
-    page_count += 1
-    is_first = False
-    page_response = requests.get(page_url)
-    page_soup = BeautifulSoup(page_response.text, "lxml")
-
-    sub_url_l = page_soup.select('div .mw-allpages-body li>a')
-    raw_text = ""
-    with open("wiki_cxk_point.txt","w+", encoding="utf-8") as p :
-        with open(f".\\Wikitext\\wiki{page_count}.txt", 'w+', encoding="utf-8") as f :
-            p.write(str(page_count)+"\n")
-            for prgraph_url in tqdm(sub_url_l) :
-                main_url ="https://en.wikipedia.org/" + prgraph_url["href"]
-                p.write(main_url+"\n")
-                main_soup = get_re(main_url)
-
-                #find paragraph of main page
-                for del_tag in main_soup.find_all("sup", class_="reference") :
-                    del_tag.decompose()
-                for del_tag in main_soup.find_all("math") :
-                    del_tag.decompose()
-                prgraph = main_soup.select("p", role="paragraph")
-                for txt in prgraph :
-                    f.write(txt.get_text())
-            page_url = "https://en.wikipedia.org" + page_soup.find_all('a', title="Special:AllPages")[1]["href"]
-f.close()  
-p.close()
--- test.py
+++ test.py
@@ -1,28 +1,4 @@
-<<<<<<< HEAD
 import multiprocessing
 import multiprocessing.pool
 
 print(multiprocessing.cpu_count())
-=======
-from bs4 import BeautifulSoup
-
-html_doc = """
-<div>
-  <h1>Hello</h1>
-  <p>
-    World
-  </p>
-</div>
-"""
-
-soup = BeautifulSoup(html_doc, 'html.parser')
-
-# 들여쓰기 없이 출력
-pretty_html = soup.prettify(formatter="html")
-print(pretty_html)
-
-
-# 들여쓰기 없이 출력
-pretty_html = soup.encode(formatter="html").decode()
-print(pretty_html)
->>>>>>> 0c1bdb6136dc2ae73963117951799add08d7f4fa
